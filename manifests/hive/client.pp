8ed846e1cc314803dd63cf2ab2afdb5ca8600045
Jeff Field <jeff.field@gmail.com>
2013-05-12 19:28:36 -0400

    Refactored Hive client module, reformatted to match puppet style guidelines.

---
+++ manifests/hive/client.pp
@@ -0,0 +1,25 @@
+# /etc/puppet/modules/hive/manifests/init.pp
+
+class cdh3::hive::client {
+
+  $mysql_driver_version='5.1.14'
+
+  package { 'hadoop-hive':
+    ensure => installed,
+  }
+  file {'hive-site.xml':
+    mode    => '0755',
+    path    => '/etc/hive/conf/hive-site.xml',
+    content => template('hive/hive-site.xml.erb'),
+    require => Package['hadoop-hive'],
+  }
+
+  file {'mysql-driver':
+    mode    => '0755',
+    path    => "/usr/lib/hive/lib/mysql-connector-java-${mysql_driver_version}-bin.jar",
+    source  => "puppet:///hive/mysql-connector-java-${mysql_driver_version}-bin.jar",
+    require => Package['hadoop-hive'],
+  }
+
+}
+
